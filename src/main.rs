fa8dfe16cdad6d034faf2ace2344c21678eaa29a
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-02-07 23:45:25 +0100

    feat: RLPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{self, Write};
+
+use monkey::{lexer::lexer::Lexer, token::token::Token};
+
+const PROMPT: &str = "@ ";
+
 fn main() {
-    println!("Monkey Programming Language !");   
+    println!("Monkey Programming Language !");
+
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let input = &mut String::new();
+
+    loop {
+        input.clear();
+        print!("{PROMPT}");
+        let _ = stdout.flush();
+        let _ = stdin.read_line(input);
+        print!("{}", input);
+
+        let mut lexer = Lexer::new(input.clone());
+
+        let mut token = lexer.next_token();
+        while token != Token::EOF {
+            println!("{:?}", token);
+            token = lexer.next_token();
+        }
+
+        println!();
+    }
 }
