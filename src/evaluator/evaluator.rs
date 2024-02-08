c463d3dc8360e9fd3273501d525c11d10bdf1b2b
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-02-08 15:34:49 +0100

    feat: Eval Boolean expressions

--- src/evaluator/evaluator.rs
+++ src/evaluator/evaluator.rs
@@ -7,12 +7,20 @@ use crate::{
     object::object::Object,
 };
 
+const TRUE: Object = Object::BOOLEAN(true);
+const FALSE: Object = Object::BOOLEAN(false);
+const NULL: Object = Object::NULL;
+
 pub fn eval(node: Program) -> Object {
-    node.statements
+    match node
+        .statements
         .iter()
         .map(|stmt| eval_statement(stmt.clone()))
         .last()
-        .unwrap()
+    {
+        Some(obj) => obj,
+        None => NULL,
+    }
 }
 
 fn eval_statement(node: Statement) -> Object {
@@ -27,11 +35,19 @@ fn eval_expression(node: Expression) -> Object {
     match node {
         Identifier(_) => todo!(),
         Integer(i) => Object::INTEGER(i),
-        Bool(b) => Object::BOOLEAN(b),
+        Bool(b) => native_bool_to_object(b),
         Prefix(_) => todo!(),
         Infix(_) => todo!(),
         IfExpression(_) => todo!(),
         FnExpression(_) => todo!(),
         CallExpression(_) => todo!(),
+    }
+}
+
+fn native_bool_to_object(b: bool) -> Object {
+    if b {
+        TRUE
+    } else {
+        FALSE
     }
 }
