8c62ca8109f7ddcdd568a69aeb559f9d194a2d1a
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-02-22 14:15:36 +0100

    feat(eval): Eval bang expressions

--- src/evaluator/evaluator.rs
+++ src/evaluator/evaluator.rs
@@ -5,6 +5,7 @@ use crate::{
         Statement::{self, *},
     },
     object::object::Object,
+    token::token::Token,
 };
 
 const TRUE: Object = Object::BOOLEAN(true);
@@ -36,11 +37,26 @@ fn eval_expression(node: Expression) -> Object {
         Identifier(_) => todo!(),
         Integer(i) => Object::INTEGER(i),
         Bool(b) => native_bool_to_object(b),
-        Prefix(_) => todo!(),
+        Prefix(p) => eval_prefix_expression(p.operator, eval_expression(*p.expr)),
         Infix(_) => todo!(),
         IfExpression(_) => todo!(),
         FnExpression(_) => todo!(),
         CallExpression(_) => todo!(),
+    }
+}
+
+fn eval_prefix_expression(operator: Token, object: Object) -> Object {
+    match operator {
+        Token::BANG => eval_bang_expression(object),
+        _ => todo!(),
+    }
+}
+
+fn eval_bang_expression(object: Object) -> Object {
+    match object {
+        TRUE => FALSE,
+        FALSE | Object::NULL | Object::INTEGER(0) => TRUE,
+        _ => FALSE,
     }
 }
 
