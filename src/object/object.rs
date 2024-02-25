2f0c83ab65d7b1a54b40994badfe6428bce0b7a4
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-02-25 17:21:58 +0100

    feat(object): Add Error Object

--- src/object/object.rs
+++ src/object/object.rs
@@ -6,6 +6,19 @@ pub enum Object {
     BOOLEAN(bool),
     NULL,
     RETURN(Box<Object>),
+    ERROR(String),
+}
+
+impl Object {
+    pub fn get_type(&self) -> String {
+        match self {
+            Object::INTEGER(_) => "INTEGER".to_string(),
+            Object::BOOLEAN(_) => "BOOLEAN".to_string(),
+            Object::NULL => "NULL".to_string(),
+            Object::RETURN(obj) => obj.get_type(),
+            Object::ERROR(_) => "ERROR".to_string(),
+        }
+    }
 }
 
 impl fmt::Display for Object {
@@ -15,6 +28,7 @@ impl fmt::Display for Object {
             Object::BOOLEAN(b) => write!(f, "{b}"),
             Object::NULL => write!(f, "null"),
             Object::RETURN(r) => write!(f, "return {r}"),
+            Object::ERROR(s) => write!(f, "{s}"),
         }
     }
 }
