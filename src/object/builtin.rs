3ed0f028775df35ad2b6c74394fec959ab0df8dd
LACENNE yanis <yanis.lacenne@etu.u-paris.fr>
2024-05-29 22:00:27 +0200

    feat(builtin): Implement builtin methods (len)

---
+++ src/object/builtin.rs
@@ -0,0 +1,41 @@
+use super::object::Object;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BuiltinFunction {
+    LEN,
+}
+
+impl BuiltinFunction {
+    pub fn get_builtin(name: &str) -> Option<Object> {
+        Self::from(name).ok()
+    }
+
+    fn from(name: &str) -> Result<Object, ()> {
+        match name {
+            "len" => Ok(Object::BUILTIN(Self::LEN)),
+            _ => Result::Err(()),
+        }
+    }
+
+    pub fn call(&self, args: Vec<Object>) -> Object {
+        match self {
+            BuiltinFunction::LEN => Self::call_len(args),
+        }
+    }
+
+    fn call_len(args: Vec<Object>) -> Object {
+        Self::handle_expected_number_arguments(1, args.len()).unwrap_or_else(|| match &args[0] {
+            Object::STRING(s) => Object::INTEGER(s.len().try_into().unwrap()),
+            _ => Object::ERROR("Argument type not supported by `len`.".to_string()),
+        })
+    }
+
+    fn handle_expected_number_arguments(expected: usize, received: usize) -> Option<Object> {
+        match expected == received {
+            true => None,
+            false => Some(Object::ERROR(format!(
+                "Wrong number of arguments. Expected: {expected} | Got: {received}"
+            ))),
+        }
+    }
+}
--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,2 +1,3 @@
 pub mod env;
 pub mod object;
+pub mod builtin;
